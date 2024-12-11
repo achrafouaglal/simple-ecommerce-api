ecac8c11e837333a06d07dee4da5f9a7bcd84aa3
achrafouaglal <ibrahimachraf0250@gmail.com>
2024-12-11 18:02:57 +0100

    a

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json());
 
 app.set("view engine","ejs")
-app.set("views","views")
+app.set("views", path.join(__dirname , "views"))
 
 app.use("/", require("./routes/index.route"))
 app.get("/add",(req,res) => {
